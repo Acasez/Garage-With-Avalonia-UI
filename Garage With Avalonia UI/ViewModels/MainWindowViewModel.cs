dcb8caa92c868936f519d54e329b6d87030191a2
Acasez <edvin.sanne@gmail.com>
2026-05-27 14:23:18 +0200

    Add project files.

---
+++ Garage With Avalonia UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Garage_With_Avalonia_UI.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Garage With Avalonia UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Garage_With_Avalonia_UI.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
