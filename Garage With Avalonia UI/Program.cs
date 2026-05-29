b7e16024d0ae0f4e10891a3dc0ae4b80c570b05d
Acasez <edvin.sanne@gmail.com>
2026-05-29 09:26:39 +0200

    Changed Porgram file

--- Garage With Avalonia UI/Program.cs
+++ Garage With Avalonia UI/Program.cs
@@ -1,25 +1,35 @@
 ﻿using Avalonia;
-using System;
+using Avalonia.Controls;
+using Avalonia.Themes.Fluent;
 
-namespace Garage_With_Avalonia_UI
+class Program
 {
-    internal sealed class Program
+    public static void Main(string[] args)
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        AppBuilder.Configure<Application>()
+                  .UsePlatformDetect()
+                  .Start(AppMain, args);
+    }
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-#if DEBUG
-                .WithDeveloperTools()
-#endif
-                .WithInterFont()
-                .LogToTrace();
+    static void AppMain(Application app, string[] args)
+    {
+        app.Styles.Add(new FluentTheme());
+
+        var window = new Window
+        {
+            Title = "Hello from Code",
+            Width = 400,
+            Height = 300,
+            Content = new TextBlock
+            {
+                Text = "No XAML here.",
+                FontSize = 24,
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            }
+        };
+
+        window.Show();
+        app.Run(window);
     }
 }
