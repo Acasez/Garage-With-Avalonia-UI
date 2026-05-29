5eb9b0d7be66b20addffe7cfd82eb6013fa0cf92
Acasez <edvin.sanne@gmail.com>
2026-05-29 09:49:37 +0200

    Not working

--- Garage With Avalonia UI/ViewModels/MainWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia.Interactivity;
-using System.Diagnostics;
-namespace Garage_With_Avalonia_UI.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to the Garage!";
-    public string Instructions { get; } = "Let's create a garage. How many spots do you want?";
-}
--- Garage With Avalonia UI/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Garage_With_Avalonia_UI.ViewModels
-{
-    public abstract class ViewModelBase : ObservableObject
-    {
-    }
-}
